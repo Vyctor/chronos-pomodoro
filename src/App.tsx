0f0b8c9801db9cf491f5f32cc8822ff79f0675ad
Vyctor <dev.vyctor@gmail.com>
2025-05-12 08:35:45 -0300

    importando e usando arquivos de CSS Modules com escopo de componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,18 @@
+import { Heading } from './components/Heading';
+
+import './styles/theme.css';
+import './styles/global.css';
+
 export function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Heading />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus,
+        accusantium velit, dignissimos eligendi repellat at doloremque magni
+        aperiam, modi repudiandae quisquam recusandae perferendis voluptas fugit
+        tempora deleniti. Facere, quod obcaecati.
+      </p>
+    </>
+  );
 }
